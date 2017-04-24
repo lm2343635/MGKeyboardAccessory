e1409a0217c987528b2b641ac8b849c316bc1360
Meng Li <lm2343635@126.com>
2017-04-24 17:22:01 +0900

    Insert string in keyboard accessory to text field.

--- Example/MGKeyboardAccessory/ViewController.swift
+++ Example/MGKeyboardAccessory/ViewController.swift
@@ -15,13 +15,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        textField.setCloseKeyboardAccessoryForSender([".com", ".cn", ".net"])
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        textField.becomeFirstResponder()
+        textField.setupKeyboardAccessory([":", "/", "\""], barStyle: .default)
     }
 
 }
--- MGKeyboardAccessory/Classes/UITextField+KeyboardAccessory.swift
+++ MGKeyboardAccessory/Classes/UITextField+KeyboardAccessory.swift
@@ -2,32 +2,33 @@ import Foundation
 
 public extension UITextField {
 
-    func setCloseKeyboardAccessoryForSender(_ strings: [String]) {
+    func setupKeyboardAccessory(_ strings: [String], barStyle: UIBarStyle) {
+        let buttonColor = (barStyle == .default) ? UIColor.darkGray : UIColor.white
+        
         let topView: UIToolbar = {
             let view = UIToolbar.init(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 35))
-            view.barStyle = .black;
-//            let clearButtonItem = UIBarButtonItem(title: NSLocalizedString("clear_name", comment: ""),
-//                                                  style: UIBarButtonItemStyle.plain,
-//                                                  target: self,
-//                                                  action: #selector(clearTextFeild))
-//            clearButtonItem.tintColor = UIColor.white
+            view.barStyle = barStyle;
+            let clearButtonItem = UIBarButtonItem(barButtonSystemItem: .trash,
+                                                  target: self,
+                                                  action: #selector(clearTextFeild))
+            clearButtonItem.tintColor = buttonColor
+            let spaceButtonItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                                  target: self,
+                                                  action: nil)
+            let doneButtonItem = UIBarButtonItem(barButtonSystemItem: .done,
+                                                 target: self,
+                                                 action: #selector(editFinish))
+            doneButtonItem.tintColor = buttonColor
             
-            
-            let spaceButtonItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-            
-            
-//            let doneButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(editFinish))
-//            doneButtonItem.tintColor = UIColor.white
-            
-//            var items = [clearButtonItem, spaceButtonItem]
-            var items = [spaceButtonItem]
+            var items = [clearButtonItem, spaceButtonItem]
             for string in strings {
                 items.append(createStringBarButtonItem(string: string,
-                                                          action: #selector(addCharacter(_:)),
-                                                          height: 26))
+                                                       color:  buttonColor,
+                                                       action: #selector(addText(_:)),
+                                                       height: 26))
             }
             items.append(spaceButtonItem)
-//            items.append(doneButtonItem)
+            items.append(doneButtonItem)
             view.setItems(items, animated: false)
             
             return view
@@ -36,30 +37,41 @@ public extension UITextField {
         self.inputAccessoryView = topView
     }
     
-    func createStringBarButtonItem(string: String, action: Selector, height: CGFloat) -> UIBarButtonItem {
+    func createStringBarButtonItem(string: String, color: UIColor, action: Selector, height: CGFloat) -> UIBarButtonItem {
         let stringButton: UIButton = {
             let button = UIButton(type: .custom)
             button.setTitle(string, for: .normal)
             var width = button.sizeThatFits(CGSize.init(width: CGFloat.greatestFiniteMagnitude, height: height)).width
-            if width > height {
+            if width > height * 1.2 {
                 width += height / 4;
             }
             button.frame = CGRect(x: 2, y: 2, width: width, height: height)
             button.layer.cornerRadius = 5
             button.layer.borderWidth = 1
-            button.layer.borderColor = UIColor.white.cgColor
-            
-            button.tintColor = UIColor.white
+            button.layer.borderColor = color.cgColor
+            button.tintColor = color
+            button.setTitleColor(color, for: .normal)
             button.addTarget(target, action: action, for: .touchUpInside)
-            
             return button
         }()
         let characterButtonItem = UIBarButtonItem(customView: stringButton)
         return characterButtonItem
     }
     
-    func addCharacter(_ sender: UIButton) {
-
+    func addText(_ sender: UIButton) {
+        self.insertText((sender.titleLabel?.text)!)
+    }
+    
+    func editFinish() {
+        if self.isFirstResponder {
+            self.resignFirstResponder()
+        }
+    }
+    
+    func clearTextFeild() {
+        if self.isFirstResponder {
+            self.text = ""
+        }
     }
     
 }
